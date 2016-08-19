5afcb75a2ef2b5dae65a6377b10ca3ae5e81f307
Antho <antho@anthonix.fr>
2016-08-19 21:51:04 +0200

    Adding rpm-build package

--- build-redis.sh
+++ build-redis.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 REDIS_VERSION=3.2.3
-#echo Downloading deps :
-#rpm -ivh  http://dl.fedoraproject.org/pub/epel/6/$(uname -m)/epel-release-6-8.noarch.rpm
-yum install gcc wget -y
+yum install gcc wget rpm-build -y
 
 wget "http://download.redis.io/releases/redis-${REDIS_VERSION}.tar.gz" -cO SOURCES/redis-${REDIS_VERSION}.tar.gz
 
-[ ! -f SOURCES/php-${REDIS_VERSION}-strip.tar.xz ] && cd SOURCES && ./strip.sh ${REDIS_VERSION} && cd ..
-
-cat SPECS/redis.spec.tpl |sed "s/TPL_REDIS_VERSION/$REDIS_VERSION/g" > SPECS/redis.spec
+cat SPECS/redis.spec.tpl | sed "s/TPL_REDIS_VERSION/$REDIS_VERSION/g" > SPECS/redis.spec
 
 rpmbuild --define "_topdir `pwd`" -bb SPECS/redis.spec
